3688443e25c1ba0c4fa463d00fb52ac9eaf58995
shanieini <shani396879@gmail.com>
2022-05-24 20:25:55 +0300

    shani files

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './root-cmp'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import './style/styles.scss'
+import './assets/styles.main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // {/* </React.StrictMode> */ }
 )
-console.log('ssss');
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
