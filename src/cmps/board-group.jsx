bd3e9782a2c903f3c9cdfcd1714647d1abcf5653
shanieini <shani396879@gmail.com>
2022-05-25 15:36:31 +0300

    shanii

--- src/cmps/board-group.jsx
+++ src/cmps/board-group.jsx
@@ -4,7 +4,11 @@ import { BoardPreview } from "./board-preview";
 
 export function BoardGroup({ items }) {
     return <div className="board-group">
-        {items.map((item, idx) => <BoardPreview item={item} key={idx} />)}
-        <input type="text" placeholder="+ Add Item" />
+        <div className="board-group-wrapper">
+            <div className="border-group-content">
+                {items.map((item, idx) => <BoardPreview item={item} key={idx} />)}
+                <input type="text" placeholder="+ Add Item" />
+            </div>
+        </div>
     </div>
 }
