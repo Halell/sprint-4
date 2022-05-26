9508a7fbce2213b32af0f195a196d40c242aeecd
Halell <halellhofman@gmail.com>
2022-05-26 11:35:43 +0300

    H

--- src/cmps/board.jsx
+++ src/cmps/board.jsx
@@ -3,7 +3,6 @@ import { BoardHeader } from './board-header'
 import { BoardGroup } from './board-group'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadBoard } from '../store/action/board.actions'
-import { boardService } from '../services/board.service'
 
 export const Board = () => {
     const { board } = useSelector((storeState) => storeState.boardModule)
@@ -12,37 +11,31 @@ export const Board = () => {
         dispatch(loadBoard())
     }, [])
 
+    const onSetTask = useCallback(async (task) => {
+        console.log(task)
+        // dispatch(setFilterBy(filterBy))
+        // dispatch(loadRobots())
+    }, [])
+
+
     return (
         <section className='board board-controller-pinned flex'>
             <BoardHeader />
             <div className="board-group">
                 <div className="board-group-wrapper">
                     <div className="border-group-content">
-<<<<<<< HEAD
-    {
-        board && board.groups?.map((group, idx) =>
-=======
-<<<<<<< HEAD
-                        {board && board.groups?.map((group, idx) => <BoardGroup group={group} key={idx} />)}
-=======
-                        {board && board.groups?.map((group, idx) =>
->>>>>>> 0e037df20b707471324a20486cbf6f1947e2bc4d
-            <BoardGroup
-                group={ group }
-                columns={ board.columns }
-                key={ idx }
+                        { board && board.groups?.map((group, idx) =>
+                            <BoardGroup
+                                onSetTask={ onSetTask }
+                                group={ group }
+                                columns={ board.columns }
+                                key={ idx }
 
-            />
-<<<<<<< HEAD
-        )
-    }
-=======
-                        )}
->>>>>>> ea8e494ca9644c5f44fab15a29d31c2b952cb0e1
->>>>>>> 0e037df20b707471324a20486cbf6f1947e2bc4d
-                    </div >
-                </div >
-            </div >
+                            />
+                        ) }
+                    </div>
+                </div>
+            </div>
         </section >
     )
 }
