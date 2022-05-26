2e4b43dcb395c6f4acef60ef38ec46c98073cf5f
Halell <halellhofman@gmail.com>
2022-05-26 15:21:42 +0300

    H

--- src/cmps/board-group.jsx
+++ src/cmps/board-group.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import { TaskList } from "./task-list"
-// import { useFormRegister } from "../hooks/useFormRegister"
-import { useDispatch, useSelector } from 'react-redux'
-import { saveTask } from '../store/action/task.actions'
+
+import { useSelector } from 'react-redux'
+
 
 export function BoardGroup({ group, columns, onAddTask }) {
-    const dispatch = useDispatch()
+
     const { board } = useSelector((storeState) => storeState.boardModule)
-    const boardId = board._id
     const [task, setTask] = useState(null)
 
     const onHandleChange = ({ target }) => {
@@ -15,7 +14,6 @@ export function BoardGroup({ group, columns, onAddTask }) {
         const value = target.type === 'number' ? (+target.value || '') : target.value
         setTask((prevFields) => ({ ...prevFields, [field]: value }))
     }
-    // console.log('from board group', task)
 
     return (
         <div className="group-header-wrppaer">
@@ -34,7 +32,7 @@ export function BoardGroup({ group, columns, onAddTask }) {
                         <div key={ idx } >{ column }</div>
                     ) }
                 </div>
-                <div className="column-wrapper-add"></div>
+                <div className="column-wrapper-add">+</div>
             </div>
             <TaskList group={ group } />
             <form onSubmit={ (ev) => onAddTask(board, group.id, task, ev) }  >
--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -4,7 +4,7 @@ export function TaskPreview({ task }) {
     // console.log(task)
     return (
         <div className="task-preview">
-            <hr />
+
             { task && task.title }
             <button className=" edit-btn">Edit</button>
         </div>
