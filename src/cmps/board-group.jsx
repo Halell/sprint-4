4cea6402b249b54131162662c77991b62c40fa9d
CarmelYona <carmelyona26@gmail.com>
2022-05-26 11:54:41 +0300

    ff

--- src/cmps/board-group.jsx
+++ src/cmps/board-group.jsx
@@ -1,57 +1,50 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { TaskList } from "./task-list"
+import { useFormRegister } from "../hooks/useFormRegister"
 import { useDispatch, useSelector } from 'react-redux'
 import { saveTask } from '../store/action/task.actions'
 
-export function BoardGroup({ group, columns, onAddTask }) {
+export function BoardGroup({ group, columns }) {
     const dispatch = useDispatch()
     const { board } = useSelector((storeState) => storeState.boardModule)
+    const boardId = board._id
     const [task, setTask] = useState(null)
 
     const onHandleChange = ({ target }) => {
         const field = target.name
         const value = target.type === 'number' ? (+target.value || '') : target.value
         setTask((prevFields) => ({ ...prevFields, [field]: value }))
-        console.log('task22: ', task);
     }
+
+    const onAddTask = (task, boardId, groupId) => {
+        console.log('saving')
+        dispatch(saveTask(task, boardId, groupId))
+    }
+
     return (
         <div className="group-header-wrppaer">
             <div className="group-header-cmp flex" >
                 <div className="column-wrapper-title flex">
-                    <div className="btn-group-menu flex">🟢</div>
-                    <div className="title-inner-container">
-                        <div>
+                    <div className="btn-group-menu flex">🟢
+                        <div className="title-inner-container">
                             <div className="group-title text-component" style={{ color: "#037f4c" }} >{group.title}</div>
                         </div>
                     </div>
-
                 </div>
                 <div className="column-wrapper-columns">
-                    {columns && columns.map((column, idx) =>
+                    {columns.map((column, idx) =>
 
                         <div key={idx} >{column}</div>
                     )}
                 </div>
-                <div className="column-wrapper-add">+</div>
+                <div className="column-wrapper-add"></div>
             </div>
-<<<<<<< HEAD
             <TaskList group={group} />
             <form onSubmit={(ev) => onAddTask(task, boardId, group.id, ev.preventDefault()
             )}  >
                 <input type="text" onChange={onHandleChange} />
-=======
-<<<<<<< HEAD
-            <TaskList group={group} />
-            <form onSubmit={(ev) => onAddTask(board, group.id, task, ev)}  >
-                <input type="text" onChange={onHandleChange} name="title" />
-=======
-            <TaskList group={ group } />
-            <form onSubmit={ (ev) => onAddTask(task, boardId, group.id, ev.preventDefault()) }  >
-
-                <input type="text" onChange={ onHandleChange } />
->>>>>>> 9508a7fbce2213b32af0f195a196d40c242aeecd
->>>>>>> 074a42602e006d89f3c581797e0f195638d60960
             </form>
         </div>
     )
+
 }
--- src/cmps/board.jsx
+++ src/cmps/board.jsx
@@ -3,11 +3,7 @@ import { BoardHeader } from './board-header'
 import { BoardGroup } from './board-group'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadBoard } from '../store/action/board.actions'
-<<<<<<< HEAD
 import { boardService } from '../services/board.service'
-import { saveTask } from '../store/action/task.actions'
-=======
->>>>>>> 9508a7fbce2213b32af0f195a196d40c242aeecd
 
 export const Board = () => {
     const { board } = useSelector((storeState) => storeState.boardModule)
@@ -16,56 +12,20 @@ export const Board = () => {
         dispatch(loadBoard())
     }, [])
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    const onAddTask = async (board, groupId, task, ev) => {
-        ev.preventDefault()
-        console.log('task11: ', task);
-        console.log('board11: ', board);
-        console.log('groupId11: ', groupId);
-        dispatch(saveTask(board, groupId, task))
-    }
-=======
-    const onSetTask = useCallback(async (task) => {
-        console.log(task)
-        // dispatch(setFilterBy(filterBy))
-        // dispatch(loadRobots())
-    }, [])
-
->>>>>>> 9508a7fbce2213b32af0f195a196d40c242aeecd
-
->>>>>>> 074a42602e006d89f3c581797e0f195638d60960
     return (
         <section className='board board-controller-pinned flex'>
             <BoardHeader />
             <div className="board-group">
                 <div className="board-group-wrapper">
                     <div className="border-group-content">
-<<<<<<< HEAD
                         {board && board.groups?.map((group, idx) =>
-=======
-                        { board && board.groups?.map((group, idx) =>
->>>>>>> 074a42602e006d89f3c581797e0f195638d60960
                             <BoardGroup
-<<<<<<< HEAD
-                                onAddTask={onAddTask}
                                 group={group}
                                 columns={board.columns}
                                 key={idx}
-=======
-                                onSetTask={ onSetTask }
-                                group={ group }
-                                columns={ board.columns }
-                                key={ idx }
->>>>>>> 9508a7fbce2213b32af0f195a196d40c242aeecd
 
                             />
-<<<<<<< HEAD
                         )}
-=======
-                        ) }
->>>>>>> 074a42602e006d89f3c581797e0f195638d60960
                     </div>
                 </div>
             </div>
