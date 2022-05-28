65af4d2f9aa0bb63c5123160d13d7bb70212e8b2
shanieini <shani396879@gmail.com>
2022-05-28 16:03:18 +0300

    ssss

--- src/cmps/board-filter.jsx
+++ src/cmps/board-filter.jsx
@@ -6,7 +6,7 @@ import { useFormRegister } from "../hooks/useFormRegister"
 export const BoardFilter = ({ onAddGroup, onChangeFilter, getPersons }) => {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [className, setClassName] = useState("")
-
+    // const filterSvg = <svg viewBox="0 0 20 20" fill="currentColor" width="20" height="20"><path d="M8.65191 2.37299C6.9706 2.37299 5.35814 3.04089 4.16927 4.22976C2.9804 5.41863 2.3125 7.03108 2.3125 8.7124C2.3125 10.3937 2.9804 12.0062 4.16927 13.195C5.35814 14.3839 6.9706 15.0518 8.65191 15.0518C10.0813 15.0518 11.4609 14.5691 12.5728 13.6939L16.4086 17.5303C16.7014 17.8232 17.1763 17.8232 17.4692 17.5303C17.7621 17.2375 17.7622 16.7626 17.4693 16.4697L13.6334 12.6333C14.5086 11.5214 14.9913 10.1418 14.9913 8.7124C14.9913 7.03108 14.3234 5.41863 13.1346 4.22976C11.9457 3.04089 10.3332 2.37299 8.65191 2.37299ZM12.091 12.1172C12.9878 11.2113 13.4913 9.98783 13.4913 8.7124C13.4913 7.42891 12.9815 6.19798 12.0739 5.29042C11.1663 4.38285 9.9354 3.87299 8.65191 3.87299C7.36842 3.87299 6.1375 4.38285 5.22993 5.29042C4.32237 6.19798 3.8125 7.42891 3.8125 8.7124C3.8125 9.99589 4.32237 11.2268 5.22993 12.1344C6.1375 13.0419 7.36842 13.5518 8.65191 13.5518C9.92736 13.5518 11.1509 13.0483 12.0568 12.1514C12.0623 12.1455 12.0679 12.1397 12.0737 12.134C12.0794 12.1283 12.0851 12.1227 12.091 12.1172Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
     const toggle = (val) => {
         if (val === 'person-modal') {
             setIsModalOpen(isModalOpen ? false : true)
@@ -32,7 +32,7 @@ export const BoardFilter = ({ onAddGroup, onChangeFilter, getPersons }) => {
             <div className="new-item-wraper flex">
                 <div onClick={() => onAddGroup()} className="new-item-btn flex">New Item</div>
                 <div className="new-item-modal-btn-wrpper">
-                    <div onClick={() => openModal()} className="new-item-modal-btn">\/</div>
+                    <div onClick={() => openModal()} className="new-item-modal-btn"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"></path></svg></div>
                 </div>
             </div>
 
@@ -45,7 +45,7 @@ export const BoardFilter = ({ onAddGroup, onChangeFilter, getPersons }) => {
             </div>
 
             <div className="search-wrapper">
-                <input className="search-input" placeholder="🔍 search" {...register('title')} />
+                <input className="search-input" placeholder="🔍 search"  {...register('title')} />
             </div>
             <div className="persons-wrapper">
                 <div className="person-btn" onClick={() => toggle('person-modal')}>persons</div>
