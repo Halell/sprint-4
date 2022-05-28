197b3495848f58260cfffb5d48475f64077c89a2
shanieini <shani396879@gmail.com>
2022-05-28 21:49:12 +0300

    dsds

--- src/cmps/board-filter.jsx
+++ src/cmps/board-filter.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { useFormRegister } from "../hooks/useFormRegister"
+// import filterSvg from '../assets/svg/filter.svg'
+import { ReactComponent as ArrowSvg } from '../assets/svg/arrow.svg'
 
 
 
@@ -31,7 +33,8 @@ export const BoardFilter = ({ onAddGroup, onChangeFilter, getPersons, board }) =
             <div className="new-item-wraper flex">
                 <div onClick={() => onAddGroup()} className="new-item-btn flex">New Item</div>
                 <div className="new-item-modal-btn-wrpper">
-                    <div onClick={() => openModal()} className="new-item-modal-btn"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"></path></svg></div>
+                    {/* <div onClick={() => openModal()} className="new-item-modal-btn"><img src={filterSvg} alt="" /></div> */}
+                    <div onClick={() => openModal()} className="new-item-modal-btn"><ArrowSvg /></div>
                 </div>
             </div>
 
