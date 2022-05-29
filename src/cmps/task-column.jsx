513e00b29ba7774e03a5baabf91d993f1d709071
CarmelYona <carmelyona26@gmail.com>
2022-05-29 21:04:08 +0300

    importance

--- src/cmps/task-column.jsx
+++ src/cmps/task-column.jsx
@@ -11,7 +11,7 @@ export function TaskColumn({ column: boardColumn, setColumn, task, isStatusEdit,
                 <div className="column-modal">
 
                     <div style={{ backgroundColor: 'rgb(0, 200, 117)' }} className="btn-wraper"> <div onClick={() => setStatus('done')} className="status-edit-btn status-edit-btn-done">done!</div></div>
-                    <div style={{ backgroundColor: 'rgb(253, 171, 61)' }} className="btn-wraper"> <div onClick={() => setStatus('in progress')} className="status-edit-btn in-progress">in progress</div></div>
+                    <div style={{ backgroundColor: 'rgb(253, 171, 61)' }} className="btn-wraper"> <div onClick={() => setStatus('in-progress')} className="status-edit-btn in-progress">in progress</div></div>
                     <div style={{ backgroundColor: 'rgb(226, 68, 92)' }} className="btn-wraper"> <div onClick={() => setStatus('stuck')} className="status-edit-btn stuck">stuck</div></div>
                     <div style={{ backgroundColor: 'grey' }} className="btn-wraper"> <div onClick={() => setStatus('no-status')} className="status-edit-btn stuck"></div>none</div>
                     <div className="btn-wraper"> <div className="status-edit-btn add-edit-labels">Add/Edit Labels</div></div>
@@ -21,6 +21,16 @@ export function TaskColumn({ column: boardColumn, setColumn, task, isStatusEdit,
         }
         {boardColumn === 'date' && <div onClick={() => setColumn(boardColumn)} className="task-column">
             {task.date}
+            {isStatusEdit &&
+                <div className="column-modal">
+
+                    <div style={{ backgroundColor: 'rgb(0, 200, 117)' }} className="btn-wraper"> <div onClick={() => setStatus('done')} className="status-edit-btn status-edit-btn-done">done!</div></div>
+                    <div style={{ backgroundColor: 'rgb(253, 171, 61)' }} className="btn-wraper"> <div onClick={() => setStatus('in-progress')} className="status-edit-btn in-progress">in progress</div></div>
+                    <div style={{ backgroundColor: 'rgb(226, 68, 92)' }} className="btn-wraper"> <div onClick={() => setStatus('stuck')} className="status-edit-btn stuck">stuck</div></div>
+                    <div style={{ backgroundColor: 'grey' }} className="btn-wraper"> <div onClick={() => setStatus('no-status')} className="status-edit-btn stuck"></div>none</div>
+                    <div className="btn-wraper"> <div className="status-edit-btn add-edit-labels">Add/Edit Labels</div></div>
+
+                </div>}
         </div>}
         {boardColumn === 'persons' && <div onClick={() => setColumn(boardColumn)} className="task-column">
             {task.persons}
