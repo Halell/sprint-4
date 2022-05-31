05d0b4a41279fdad48e8e06c26c5b261620c07b5
CarmelYona <carmelyona26@gmail.com>
2022-05-31 17:30:16 +0300

    header

--- src/cmps/board-header.jsx
+++ src/cmps/board-header.jsx
@@ -1,20 +1,14 @@
 import { Link } from "react-router-dom"
 import { useState } from "react"
-
-
 import { updateBoard } from "../store/action/board.actions"
 import { BoardFilter } from "./board-filter"
 import { ReactComponent as StarSvg } from '../assets/svg/star.svg'
 import { ReactComponent as UserSvg } from '../assets/svg/user.svg'
 import { ReactComponent as HideSecondSvg } from '../assets/svg/hide.second.svg'
 import { FiUserPlus } from 'react-icons/fi'
-<<<<<<< HEAD
-=======
 import { ReactComponent as ActivitySvg } from '../assets/svg/activity.svg'
-import { useState } from "react"
 import { ActivityLog } from "./acttivity"
 import { boardService } from "../services/board.service"
->>>>>>> bc5a30169d197b6d18fac0f1fdaa267d26ee00f8
 
 
 export function BoardHeader({ onAddGroup, onChangeFilter, getPersons, onSaveBoard, board, onAddTask, group }) {
@@ -67,10 +61,10 @@ export function BoardHeader({ onAddGroup, onChangeFilter, getPersons, onSaveBoar
                                 <div className="board-name flex">
                                     <div className="board-title">
                                         <div className="board-title-name"
-                                            suppressContentEditableWarning={ true }
-                                            contentEditable={ true }
-                                            onBlur={ updateBoardTitle }
-                                        >{ board.title }</div>
+                                            suppressContentEditableWarning={true}
+                                            contentEditable={true}
+                                            onBlur={updateBoardTitle}
+                                        >{board.title}</div>
                                     </div>
                                     <div>
                                         <div className="btn-title-name" >
@@ -83,78 +77,61 @@ export function BoardHeader({ onAddGroup, onChangeFilter, getPersons, onSaveBoar
                                 </div>
                             </div>
                             <div className="right-container flex">
-<<<<<<< HEAD
                                 <div className="invite"><FiUserPlus />Invite/1</div>
-                                <div onClick={ () => openActivty() } className="activity">
-                                    Activity
-=======
-                                <div onClick={() => setInvite(isMemberInvite ? false : true)} className="invite"><FiUserPlus />Invite/1</div>
-                                {isMemberInvite &&
-                                    <div className="board-invite-modal">
-                                        <h1><FiUserPlus />Invite/1</h1>
-                                        <form onSubmit={onSubmit}>
-                                            <input
-                                                className="invite-input"
-                                                type='text'
-                                                placeholder="Invite by email"
-                                                value={user.fullname}
-                                                name='fullname'
-                                                onChange={handleChange}>
-                                            </input>
-                                        </form>
-                                    </div>
-                                }
                                 <div onClick={() => openActivty()} className="activity">
-                                    <ActivitySvg /> Activity
->>>>>>> bc5a30169d197b6d18fac0f1fdaa267d26ee00f8
+                                    Activity
+                                    <div onClick={() => setInvite(isMemberInvite ? false : true)} className="invite"><FiUserPlus />Invite/1</div>
+                                    {isMemberInvite &&
+                                        <div className="board-invite-modal">
+                                            <h1><FiUserPlus />Invite/1</h1>
+                                            <form onSubmit={onSubmit}>
+                                                <input
+                                                    className="invite-input"
+                                                    type='text'
+                                                    placeholder="Invite by email"
+                                                    value={user.fullname}
+                                                    name='fullname'
+                                                    onChange={handleChange}>
+                                                </input>
+                                            </form>
+                                        </div>
+                                    }
+                                    <div onClick={() => openActivty()} className="activity">
+                                        <ActivitySvg /> Activity
+                                    </div>
+                                    <div className="add-to-board">+ Add to board </div>
+                                    <div className="edit"></div>
                                 </div>
-                                <div className="add-to-board">+ Add to board </div>
-                                <div className="edit"></div>
+                            </div>
+                            <div className="bottom-top-container">
+                                <div className="desc"
+                                    suppressContentEditableWarning={true}
+                                    contentEditable={true}
+                                    onBlur={updateBoardDesc}
+                                >{board.desc ? board.desc : 'Add board description'} </div>
                             </div>
                         </div>
-                        <div className="bottom-top-container">
-                            <div className="desc"
-                                suppressContentEditableWarning={ true }
-                                contentEditable={ true }
-                                onBlur={ updateBoardDesc }
-                            >{ board.desc ? board.desc : 'Add board description' } </div>
-                        </div>
-                    </div>
-                    {/* <div className="board-header-top-bottom flex" >
-                        <div className="btn-defult-view" >
-                            main table
-                        </div>
-                    </div> */}
-                </div>
-                {/* <div className="board-header-bottom"> */ }
-                <hr />
-                <div className="board-header-filter flex">
-                    <BoardFilter
-                        onAddGroup={ onAddGroup }
-                        onChangeFilter={ onChangeFilter }
-                        getPersons={ getPersons }
-                        board={ board }
-                        onAddTask={ onAddTask }
-                    />
-                </div>
-                {/* </div> */ }
-            </div>
-
-<<<<<<< HEAD
-            { isActivityOpen &&
-
-                <div className="activity-log-pannel">
-                    <div className="">
 
                     </div>
 
+                    <hr />
+                    <div className="board-header-filter flex">
+                        <BoardFilter
+                            onAddGroup={onAddGroup}
+                            onChangeFilter={onChangeFilter}
+                            getPersons={getPersons}
+                            board={board}
+                            onAddTask={onAddTask}
+                        />
+                    </div>
                 </div>
-
-=======
-            {isActivityOpen &&
-                <ActivityLog board={board} setActivityOpen={setActivityOpen} />
->>>>>>> bc5a30169d197b6d18fac0f1fdaa267d26ee00f8
-            }
-        </div >
+                {isActivityOpen &&
+                    <div className="activity-log-pannel">
+                        <div className="">
+                        </div>
+                    </div>
+                }
+            </div >
+        </div>
     )
 }
